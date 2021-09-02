d5916733fe0b685551b2490b50482d0d6ca4e579
BhavanSekar <61140437+BhavanSekar@users.noreply.github.com>
2021-09-03 00:05:36 +0530

    Add files via upload

---
+++ Images/add_num.py
@@ -0,0 +1,10 @@
+# This program adds two numbers
+
+NUM_1 = 1.5
+NUM_2 = 6.3
+
+# Add two numbers
+SUM = NUM_1 + NUM_2
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(NUM_1, NUM_2, SUM))

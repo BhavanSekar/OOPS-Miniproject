22fe5f6eafbb7f0497036c72e2aca4f59004ed84
BhavanSekar <61140437+BhavanSekar@users.noreply.github.com>
2021-09-03 00:44:06 +0530

    Update add_num.py

--- Images/add_num.py
+++ Images/add_num.py
@@ -1,11 +1,8 @@
-# This program adds two numbers
 
-def my_function():
     '''Demonstrates triple double quotes
     docstrings and does nothing really.'''
     NUM_1 = 1.5
     NUM_2 = 6.3
 
     # Add two numbers
-    SUM = NUM_1 + NUM_2
-return SUM    
+    SUM = NUM_1 + NUM_2   

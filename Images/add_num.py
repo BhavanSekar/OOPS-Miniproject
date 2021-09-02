8ea1ff54399235326a1ba46a5a8e420e4f6cdcf5
BhavanSekar <61140437+BhavanSekar@users.noreply.github.com>
2021-09-03 00:45:43 +0530

    Update add_num.py

--- Images/add_num.py
+++ Images/add_num.py
@@ -1,8 +1,7 @@
+'''Demonstrates triple double quotes
+docstrings and does nothing really.'''
+NUM_1 = 1.5
+NUM_2 = 6.3
 
-    '''Demonstrates triple double quotes
-    docstrings and does nothing really.'''
-    NUM_1 = 1.5
-    NUM_2 = 6.3
-
-    # Add two numbers
-    SUM = NUM_1 + NUM_2   
+# Add two numbers
+SUM = NUM_1 + NUM_2   
